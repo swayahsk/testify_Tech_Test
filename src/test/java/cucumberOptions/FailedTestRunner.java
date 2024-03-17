d827eac5e6b2e244158157a2c57e84b8c6da9e84
skandar swayah <skandarswayah0@gmail.com>
2024-03-18 00:53:20 +0100

    After adding reports

--- src/test/java/cucumberOptions/FailedTestRunner.java
+++ src/test/java/cucumberOptions/FailedTestRunner.java
@@ -5,12 +5,12 @@ import org.testng.annotations.DataProvider;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions(features="src/test/java/features",
+@CucumberOptions(features="@target/failed_scenarios.txt",
 glue="StepDefinitions",monochrome=true,
 plugin = {"pretty", "html:target/cucumber.html",
-		"json:target/cucumber.json","com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
-		"rerun:target/failed_scenarios.txt"})
-public class testNGTestRunner extends AbstractTestNGCucumberTests {
+		"json:target/cucumber.json","com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"
+		})
+public class FailedTestRunner extends AbstractTestNGCucumberTests {
 
 	@Override
 	@DataProvider(parallel=true )
